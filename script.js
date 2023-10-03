d232cee6f2479f0f826e9e94ff831afbbfcd3269
RitamCode <riram.stalltion27@gmail.com>
2023-10-03 16:55:28 +0530

    initial portfolio website code in html css js

---
+++ script.js
@@ -0,0 +1,5 @@
+// JavaScript to trigger the animation when the page loads
+window.addEventListener('load', function () {
+    const header = document.querySelector('.animation');
+    header.classList.add('animation');
+});
